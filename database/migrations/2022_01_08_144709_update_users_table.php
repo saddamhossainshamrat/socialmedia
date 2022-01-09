9ab55c604a72b15e2934d798071fdb24afbd24fc
unknown <hossainsaddam1501@gmail.com>
2022-01-09 17:34:16 +0600

    Social Media clone Project user authentication done

---
+++ database/migrations/2022_01_08_144709_update_users_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function(Blueprint $table){
+
+            $table->after('name', function($table){
+
+                $table->renameColumn('name', 'fname');
+                $table->string('lname');
+                $table->string('sex');
+                $table->string('b_day')->default('2022-01-01');
+                $table->string('image');
+
+
+            });
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
